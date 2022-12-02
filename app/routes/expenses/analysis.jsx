73ea0c667264f90a3a6e09d787221b15c6caf43a
Alex Johnson <johnsonav1992@gmail.com>
2022-12-02 06:47:17 -0600

    analysis

--- app/routes/expenses/analysis.jsx
+++ app/routes/expenses/analysis.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import ExpenseStatistics from '~/components/expenses/ExpenseStatistics'
+import Chart from '~/components/expenses/Chart'
+
+const DUMMY_EXPENSES =[
+{
+    id: 'e1',
+    title: 'First Expense',
+    amount: 12.99,
+    date: new Date().toISOString()
+},
+{
+    id: 'e2',
+    title: 'Second Expense',
+    amount: 14.99,
+    date: new Date().toISOString()
+}
+]
+
 const ExpensesAnalysisPage = () => {
     return (
-        <div>Expenses</div>
+        <main>
+            <Chart expenses={DUMMY_EXPENSES}/>
+            <ExpenseStatistics expenses={DUMMY_EXPENSES}/>
+
+        </main>
     )    
 }
 
