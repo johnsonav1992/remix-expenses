061ac40a049b4ad5d6267b62ac8dcba3985b7bf4
Alex Johnson <johnsonav1992@gmail.com>
2022-12-04 06:41:22 -0600

    add expenses and dl expenses buttons

--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -1,4 +1,6 @@
-import { Outlet } from '@remix-run/react'
+import { Outlet, Link } from '@remix-run/react'
+import { FaPlus, FaDownload } from 'react-icons/fa'
+
 import ExpensesList from '~/components/expenses/ExpensesList'
 
 const DUMMY_EXPENSES = [
@@ -21,6 +23,16 @@ const ExpensesLayout = () => {
         <>
             <Outlet />
             <main>
+                <section id="expenses-actions">
+                    <Link to="add">
+                        <FaPlus />
+                        <span>Add Expense</span>
+                    </Link>
+                    <a href="/expenses/raw">
+                        <FaDownload />
+                        <span>Load Raw Data</span>
+                    </a>
+                </section>
                 <ExpensesList expenses={DUMMY_EXPENSES}/>
             </main>
         </>
