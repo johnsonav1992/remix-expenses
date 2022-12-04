a498900c2ab631c03cd3b2a708c6da682eb69e3f
Alex Johnson <johnsonav1992@gmail.com>
2022-12-04 07:15:33 -0600

    add expense code setup

---
+++ app/data/expenses.server.js
@@ -0,0 +1,17 @@
+import { prisma } from "./database.server"
+
+export const addExpense = async (expenseData) => {
+    try {
+        return await prisma.expense.create(
+            {
+                data: {
+                    title: expenseData.title,
+                    amount: +expenseData.amount,
+                    date: new Date(expenseData.date)
+                }
+            }
+        )
+    } catch (err) {
+        console.error(err)
+    }
+}
