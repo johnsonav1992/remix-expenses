f9600264fa75711f1d1a76b6e56202408241a3af
Alex Johnson <johnsonav1992@gmail.com>
2022-12-04 13:37:51 -0600

    server side validation

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -5,6 +5,7 @@ import ExpenseForm from '~/components/expenses/ExpenseForm'
 import Modal from '~/components/util/Modal'
 import { addExpense } from '~/data/expenses.server'
 import { redirect } from '@remix-run/node'
+import { validateExpenseInput } from '~/data/validation.server'
 
 const AddExpensesPage = () => {
     const navigate = useNavigate()
@@ -23,6 +24,13 @@ const AddExpensesPage = () => {
 export const action = async ({ request }) => {
     const formData = await request.formData()
     const expenseData = Object.fromEntries(formData)
+    
+    try {
+        validateExpenseInput(expenseData)
+    } catch(err) {
+        return err
+    }
+    
     await addExpense(expenseData)
 
     return redirect('/expenses')
