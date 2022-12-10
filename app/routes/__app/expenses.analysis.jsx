b803a947cedc003ee6c82b29c329203e8812e6b7
Alex Johnson <johnsonav1992@gmail.com>
2022-12-10 06:08:06 -0600

    analysis finished and DB setup for auth

--- app/routes/__app/expenses.analysis.jsx
+++ app/routes/__app/expenses.analysis.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useLoaderData } from '@remix-run/react'
+import { useCatch, useLoaderData } from '@remix-run/react'
+import { json } from '@remix-run/node'
 
 import ExpenseStatistics from '~/components/expenses/ExpenseStatistics'
 import Chart from '~/components/expenses/Chart'
 import { getExpenses } from '~/data/expenses.server'
+import Error from '~/components/util/Error'
 
 const ExpensesAnalysisPage = () => {
     const expenses = useLoaderData()
@@ -18,7 +20,26 @@ const ExpensesAnalysisPage = () => {
 
 export const loader = async () => {
     const expenses = await getExpenses()
+
+    if (!expenses || expenses.length === 0) {
+        throw json( { message: 'Could not load expenses analysis'}, {
+            status: 404,
+            statusText: 'Expenses not found.'
+        })
+    }
+
     return expenses
+}
+
+export const CatchBoundary = () => {
+    const caughtResponse = useCatch()
+
+    return <main>
+        <Error title={caughtResponse.statusText}>
+            <p>{caughtResponse.data?.message ||
+            'Something went wrong'}</p>
+        </Error>
+    </main>
 }
 
 export default ExpensesAnalysisPage
